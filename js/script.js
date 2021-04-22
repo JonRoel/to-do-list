29a9438f3559be86b07dd429076d8982fbd8f049
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-04-22 13:38:14 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+function newItem(){
+
+  //javascript
+  //1. Adding a new item to the list of items: 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if ($('#input').val() === '') {
+      alert("Task must not be empty!")
+    } else {
+      $('#list').append(li);
+      $('#input').val('') 
+    }
+  
+   //2. Crossing out an item from the list of items:
+  
+    li.on('dblclick', function crossOut() {
+      li.toggleClass('strike')
+    });
+  
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+  
+     crossOutButton.on("click", deleteListItem);
+     function deleteListItem(){
+        li.remove();
+      }
+  
+   // 4. Reordering the items: 
+     $('#list').sortable();
+  }
+  
